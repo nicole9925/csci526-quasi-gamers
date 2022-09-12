156a0cf56c31eea7ffac1652465a14f3176c02be
Lizzet Clifton <lizzetclifton@Lizzets-MBP-306.lan>
2022-09-12 14:17:14 -0700

    launchpad feature working

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Launcher : MonoBehaviour
-{
+public class Launcher : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         
@@ -12,5 +11,15 @@ public class Launcher : MonoBehaviour
     // Update is called once per frame
     void Update() {
         
+    }
+
+    //if anything touches it, launch it straight up
+    void OnTriggerEnter(Collider collider) {
+        GameObject otherObj = collider.gameObject;
+        // Debug.Log("Collided with: " + otherObj);
+
+        Rigidbody _rb = otherObj.GetComponent<Rigidbody>();
+
+        _rb.AddForce(0, _rb.mass*1000, 0);
     }
 }
