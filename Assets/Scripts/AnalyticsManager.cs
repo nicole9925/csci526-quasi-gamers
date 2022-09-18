ac5d9a0a43a7d27bb6d94fe6c67974299075d3ad
Mridul Goyal <mridul@Mriduls-Air.lan>
2022-09-18 10:24:46 -0700

    added rest api calls

--- Assets/Scripts/AnalyticsManager.cs
+++ Assets/Scripts/AnalyticsManager.cs
@@ -2,18 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.Networking;
 
 public class AnalyticsManager 
 {
+    private int data = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //StartCoroutine(GetRequests("https://analytics-quasi-gamers.wl.r.appspot.com/update/" + data.ToString()));
     }
-
    
     public void userStartFunction()
     {
         Console.WriteLine("I am accounted in analytics duhh!!");
+        GetRequests(("https://analytics-quasi-gamers.wl.r.appspot.com/update/" + data.ToString()));
     }
+
+    //void GetData() => StartCoroutine(GetData_Coroutine());
+
+    private IEnumerator GetRequests(string uri){
+        using(UnityWebRequest request = UnityWebRequest.Get(uri))
+        {
+            yield return request.SendWebRequest();
+            if (request.result != UnityWebRequest.Result.Success)
+                Console.WriteLine(request.error);
+            else
+                Console.WriteLine(request.downloadHandler.text);
+        }
+    }
+
+    private IEnumerator PostRequest(){
+        WWWForm form = new WWWForm();
+        form.AddField("data", "1");
+
+        using (UnityWebRequest request = UnityWebRequest.Post("https://analytics-quasi-gamers.wl.r.appspot.com/update/", form))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
+    }
+    
 }
