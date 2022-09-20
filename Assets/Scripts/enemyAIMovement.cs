f617bb066c8337b78ad540914e8909d90a3f7096
Kai <kwang339@wisc.edu>
2022-09-20 10:46:50 -0700

    add enemy ai agent movement

---
+++ Assets/Scripts/enemyAIMovement.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class enemyAIMovement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+    // Start is called before the first frame update
+
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    public float sightRange;
+    public bool playerInSightRange;
+
+    private void Awake()
+    {
+        // player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Patroling();
+        // playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        //
+        // if (!playerInSightRange) Patroling();
+        // else ChasePlayer();
+    }
+    
+    private void Patroling()
+    {
+        if (!walkPointSet)
+        {
+            searchWalkPoint();
+        }
+        else
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+            
+    }
+
+    private void searchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        print("randomz: " + randomZ);
+        print("randomx: " + randomX);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround)) walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+}
