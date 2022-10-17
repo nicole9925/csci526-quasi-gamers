c9841a8b14b200ca310fa07f20d0b9c0502391b9
Rashi-Sinha <58881992+Rashi-Sinha@users.noreply.github.com>
2022-10-16 22:20:12 -0700

    level 7 bug fix
    Fixed the bug where we were not able to win by using the Hole 2.

--- Assets/Scripts/ElevatorRamp.cs
+++ Assets/Scripts/ElevatorRamp.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class ElevatorRamp : MonoBehaviour
 {
-    public float bottomFloor = -5.5f;
+    public float bottomFloor = 11f;
     public float speed = 1.5f;
+    public float maxHeight =14.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +18,8 @@ public class ElevatorRamp : MonoBehaviour
     {   
 
         Vector3 pos = transform.position;
-        float maxHeight = 12.0f;
-        float bottomFloor = -5.5f;
+        //float maxHeight = 22.0f;
+        //float bottomFloor =11f;
         pos.y = Mathf.PingPong(Time.time*speed,  maxHeight) + bottomFloor;
         transform.position = pos; // new position
         
