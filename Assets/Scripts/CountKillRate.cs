24b55260748450febdca31937ad353f2741ffc93
Kai <kwang339@wisc.edu>
2022-11-15 18:40:11 -0800

    add time record to count kill rate

--- Assets/Scripts/CountKillRate.cs
+++ Assets/Scripts/CountKillRate.cs
@@ -7,10 +7,15 @@ public class CountKillRate : MonoBehaviour
     public int killCount = 0;
     private int numEnemies;
     public GameOverLabel gameOverLabel;
+    private float timeSpent = 0;
 
     void Start()
     {
         numEnemies = GameObject.FindGameObjectsWithTag("enemy").Length;
+    }
+
+    void Update() {
+        timeSpent += Time.deltaTime*1;
     }
 
     public void addKill()
@@ -18,6 +23,11 @@ public class CountKillRate : MonoBehaviour
         killCount++;
         if (killCount == numEnemies)
         {
+            Debug.Log(timeSpent);
+
+//             Leaderboard.AddToLeaderboard(PlayerName.name, 0, (int)timeSpent);
+
+
             gameOverLabel.deactivateGameOver();
             gameOverLabel.showLabel();
         }
