b900af7286ea23b50580d1066b7ddc044d6d68dc
Nicole <nicolemandylee8@gmail.com>
2022-11-22 17:49:40 -0800

    fix falling bug

--- Assets/Scripts/EnemySpeedController.cs
+++ Assets/Scripts/EnemySpeedController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemySpeedController : MonoBehaviour
 {
     private Rigidbody _rb;
+    private bool grounded;
 
     public float speedMultiplier;
     // Start is called before the first frame update
@@ -16,7 +17,16 @@ public class EnemySpeedController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (_rb.velocity.y < 0)
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 2))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+            grounded = true;
+        } else
+        {
+            grounded = false;
+        }
+        if (_rb.velocity.y < 0 && grounded)
         {
             _rb.velocity += Vector3.up * Physics2D.gravity.y * (speedMultiplier - 1) * Time.deltaTime;
         }
