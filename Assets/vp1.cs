529fda1d408fd9a14106266057dacb061ce34a69
Kai Wang <kwang339@wisc.edu>
2022-11-29 11:42:13 -0800

    try to fix power-up videos

---
+++ Assets/vp1.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Video;
+
+public class vp1 : MonoBehaviour
+{
+    public GameObject videoPlayer;
+
+    public string videoname;
+
+    public string url;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.gameObject.GetComponent<VideoPlayer>().url = url;
+        // videoPlayer.gameObject.GetComponent<VideoPlayer>().url = System.IO.Path.Combine (Application.streamingAssetsPath,videoname);
+
+        // var loadingRequest = UnityWebRequest.Get(Path.Combine(Application.streamingAssetsPath, "your.bytes"));
+        // loadingRequest.SendWebRequest();
+        // while (!loadingRequest.isDone) {
+        //     if (loadingRequest.isNetworkError || loadingRequest.isHttpError) {
+        //         break;
+        //     }
+        // }
+        // if (loadingRequest.isNetworkError || loadingRequest.isHttpError) {
+        //
+        // } else {
+        //     File.WriteAllBytes(Path.Combine(Application.persistentDataPath , "your.bytes"), loadingRequest.downloadHandler.data);
+        // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Play();
+        }
+       
+    }
+    
+    void Play()
+    {
+        videoPlayer.gameObject.GetComponent<VideoPlayer>().Play();
+        videoPlayer.gameObject.GetComponent<VideoPlayer>().isLooping = true;
+    }
+}
